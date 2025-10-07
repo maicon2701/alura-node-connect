f7c5915c910798b7f1f598dd595943f4c78b485d
maicon-at-235719508298 <maicond2701@gmail.com>
2025-10-06 22:37:23 -0300

    feat: criando um função assincrona para ler um arquivo

--- scripts.js
+++ scripts.js
@@ -4,3 +4,22 @@ const inputUpload = document.getElementById('image-upload');
 uploadBtn.addEventListener('click',() => {
     inputUpload.click();
 })
+
+function lerConteudoDoArquivo(arquivo){
+    //O new promise é uma maneira de criar uma nova instancia de uma promisse que é usado para lidar com operações assíncronas no JavaScript.(O termo assíncrono refere-se a uma operação ou execução que ocorre de forma independente do fluxo principal de um programa, permitindo que outras partes do código sejam executadas enquanto essa operação está em andamento)
+    //A palavra-chave new em JavaScript é usada para criar uma nova instância de um objeto a partir de uma função construtora. Basicamente, ela serve para instanciar objetos que têm propriedades e métodos definidos em uma função
+    return new Promise((resolve, reject) => {
+        //A função readAsDataURL é um método do objeto FileReader em JavaScript, que é usado para ler o conteúdo de arquivos locais. Ao chamar readAsDataURL, o arquivo é convertido em uma URL de dados (data URL), que pode ser usada, por exemplo, para exibir imagens ou outros tipos de arquivos diretamente em um navegador.
+        const leitor = new FileReader();
+        leitor.onload = () =>{
+            resolve({url: leitor.result, nome:arquivo.name})
+        }
+
+        leitor.onerror = () => {
+            reject(`Erro na leitura do arquivo ${arquivo.name}`)
+
+        }
+
+        leitor.readAsDataURL(arquivo)
+    })
+}
