76bf77ad32d532584a5417abe7a1649cd4f40f4e
maicon-at-235719508298 <maicond2701@gmail.com>
2025-10-08 19:40:32 -0300

    feat: adicionado nova funcionalidade no código

--- scripts.js
+++ scripts.js
@@ -10,16 +10,40 @@ function lerConteudoDoArquivo(arquivo){
     //A palavra-chave new em JavaScript é usada para criar uma nova instância de um objeto a partir de uma função construtora. Basicamente, ela serve para instanciar objetos que têm propriedades e métodos definidos em uma função
     return new Promise((resolve, reject) => {
         
+        // Cria um novo leitor de arquivo
         const leitor = new FileReader();
-        leitor.onload = () =>{
-            resolve({url: leitor.result, nome:arquivo.name})
-        }
+        leitor.onload = () => {
+            // Resolve a Promise com um objeto contendo a URL e o nome do arquivo
+            resolve({ url: leitor.result, nome: arquivo.name });
+        };
 
         leitor.onerror = () => {
-            reject(`Erro na leitura do arquivo ${arquivo.name}`)
-
-        }
-        //A função readAsDataURL é um método do objeto FileReader em JavaScript, que é usado para ler o conteúdo de arquivos locais. Ao chamar readAsDataURL, o arquivo é convertido em uma URL de dados (data URL), que pode ser usada, por exemplo, para exibir imagens ou outros tipos de arquivos diretamente em um navegador.
+            // Rejeita a Promise com uma mensagem de erro personalizada
+            reject(`Erro na leitura do arquivo ${arquivo.name}`);
+        };
+        //A função readAsDataURL é um método do objeto FileReader em JavaScript, que é usado para ler o conteúdo de arquivos locais. Ao chamar readAsDataURL, o arquivo é convertido em uma URL de dados (data URL: base64), que pode ser usada, por exemplo, para exibir imagens ou outros tipos de arquivos diretamente em um navegador.
         leitor.readAsDataURL(arquivo)
     })
 }
+
+const imagemPrincipal = document.querySelector(".main-imagem")
+const nomeDaImagem = document.querySelector(".container-imagem-nome p")
+
+//O async informa que a função é assincrona
+inputUpload.addEventListener("change", async (evento) =>{
+    //Essa linha serve para "entrar" no evento e pegar o arquivo que está sendo enviado
+    const arquivo = evento.target.files[0]
+
+    if(arquivo){
+        //Se o carregamento do arquivo der certo ele vai colocar a imagem carregada e o nome da imagem no projeto.
+        try{
+            //O await é usado para esperar a promisse ser completada antes de continuar com o próximo passo.
+            const conteudoDoArquivo = await lerConteudoDoArquivo(arquivo);    
+            imagemPrincipal.src = conteudoDoArquivo.url;
+            nomeDaImagem.textContent = conteudoDoArquivo.nome;
+        //Se der errado vai retornar a mensagem abaixo
+        } catch (error){
+            console.log("Erro na leitura do arquivo")
+        }
+    }
+})
