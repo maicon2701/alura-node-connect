655dab5ef3ee3c2ab870a60c5786edccd426c421
Maicon Pereira <maicond2701@gmail.com>
2025-11-17 17:28:24 -0300

    feat: adicionando a função de limpar formulario

--- scripts.js
+++ scripts.js
@@ -106,20 +106,6 @@ inputTags.addEventListener('keypress', async (evento) =>{
 
 const botaoPublicar = document.querySelector(".botao-publicar");
 
-botaoPublicar.addEventListener("click", async (evento) => {
-    evento.preventDefault();
-    const nomeDoProjeto = document.getElementById("nome").value;
-    const descricaoDoProjeto = document.getElementById("descricao").value;
-    //Array.from cria um novo array a partir  de um objeto semelhante a um array.
-    //.map() é uma função de array que cria um novo array preenchido com os resultados de uma função fornecida para cada elemento do array de origem. É uma maneira eficiente de transformar dados em um areray de maneira funcional.
-    const tagsProjeto = Array.from(listaTags.querySelectorAll("p")).map((tag) => tag.textContent)
-
-    console.log(nomeDoProjeto)
-    console.log(descricaoDoProjeto)
-    console.log(tagsProjeto)
-
-})
-
 async function publicarProjeto(nomeDoProjeto,descricaoDoProjeto,tagsProjeto){
     return new Promise ((resolve,reject) =>{
         setTimeout(() => {
@@ -133,3 +119,37 @@ async function publicarProjeto(nomeDoProjeto,descricaoDoProjeto,tagsProjeto){
         }, 2000);
     })
 }
+
+
+botaoPublicar.addEventListener("click", async (evento) => {
+    evento.preventDefault();
+    const nomeDoProjeto = document.getElementById("nome").value;
+    const descricaoDoProjeto = document.getElementById("descricao").value;
+    //Array.from cria um novo array a partir  de um objeto semelhante a um array.
+    //.map() é uma função de array que cria um novo array preenchido com os resultados de uma função fornecida para cada elemento do array de origem. É uma maneira eficiente de transformar dados em um areray de maneira funcional.
+    const tagsProjeto = Array.from(listaTags.querySelectorAll("p")).map((tag) => tag.textContent)
+
+    try{
+        const resultado = await publicarProjeto(nomeDoProjeto,descricaoDoProjeto,tagsProjeto)
+        console.log(resultado)
+        alert("Deu tudo certo")
+    }catch (error){
+        console.error("Deu errado:", error)
+        alert("Deu tudo errado")
+    }
+
+})
+
+const botaoDescartar = document.querySelector(".botao-descartar");
+
+botaoDescartar.addEventListener("click", (evento) => {
+    evento.preventDefault();
+
+    const formulario = document.querySelector("form");
+    formulario.reset(); 
+
+    imagemPrincipal.src = "./img/imagem1.png";
+    nomeDaImagem.textContent = "image_projeto.png"
+    listaTags.innerHTML = "";
+
+})
